37b6a0f1d062d13f55670b5ab6a773e439ac7a8f
yohaim <yohaim@sou.edu>
2018-11-13 11:25:19 -0800

    ViewController.swift files created for all storyboard ViewControllers

---
+++ iPixel/ImgSendViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImgSendViewController.swift
+//  iPixel
+//
+//  Created by Miles Yohai on 11/13/18.
+//  Copyright © 2018 Miles Yohai. All rights reserved.
+//
+
+import UIKit
+
+class ImgSendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
